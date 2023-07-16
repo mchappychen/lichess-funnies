4b67aceec56565c5014a27939aaca8168b565118
Michael <51720724+mchappychen@users.noreply.github.com>
2023-07-15 23:55:18 -0400

    Update lichess.user.js

--- lichess.user.js
+++ lichess.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lichess Funnies
-// @version      12
-// @description  Lichess Hints
+// @version      13
+// @description  Plays chess for you on lichess
 // @author       Michael and Ian
 // @match        https://lichess.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=lichess.org
@@ -12,44 +12,48 @@
 // @require      https://raw.githubusercontent.com/mchappychen/lichess-funnies/main/chess.js
 // @require      https://raw.githubusercontent.com/mchappychen/lichess-funnies/main/stockfish.js
 // ==/UserScript==
-/* globals jQuery, $, waitForKeyElements, Chess, stockfish, lichess */
+/* globals jQuery, $, waitForKeyElements, Chess, stockfish, lichess, game */
 
+//Don't run in lobby
+if (window.location.href == 'https://lichess.org/'){return}
 
-setTimeout(function() {
+//Stockfish engine and FEN generator
+window.game = new Chess();
 
-    //Don't run in lobby
-    if (window.location.href == 'https://lichess.org/'){return}
+//Set variable for autorun
+var autoRun = localStorage.getItem('autorun') ?? "0";
+console.log(autoRun);
 
-    //Stockfish engine and FEN generator
-    var game = new Chess();
-
-    //Returns xy coord for arrow
-    function getArrowCoords(square,color){
-        //square is like "a1"
-        let bottom = square.substring(0,1).toLowerCase();
-        let x = bottom == "a" ? -3.5 :
-        (bottom == "b" ? -2.5 :
-         (bottom == "c" ? -1.5 :
-          (bottom == "d" ? -0.5 :
-           (bottom == "e" ? 0.5 :
-            (bottom == "f" ? 1.5 :
-             (bottom == "g" ? 2.5 : 3.5))))));
-        let right = square.substring(1,2);
-        let y = right == "1" ? 3.5 :
-        (right == "2" ? 2.5 :
-         (right == "3" ? 1.5 :
-          (right == "4" ? 0.5 :
-           (right == "5" ? -0.5 :
-            (right == "6" ? -1.5 :
-             (right == "7" ? -2.5 : -3.5))))));
-        //if you're black, invert them
-        if (color == "black"){x = -x;y = -y;}
-        return [x,y];
-    }
+function run(){
 
     //For puzzles
     if (window.location.href.startsWith('https://lichess.org/training')){
-        window.puzzle = function(){
+
+        //Returns xy coord for arrow
+        function getArrowCoords(square,color){
+            //square is like "a1"
+            let bottom = square.substring(0,1).toLowerCase();
+            let x = bottom == "a" ? -3.5 :
+            (bottom == "b" ? -2.5 :
+             (bottom == "c" ? -1.5 :
+              (bottom == "d" ? -0.5 :
+               (bottom == "e" ? 0.5 :
+                (bottom == "f" ? 1.5 :
+                 (bottom == "g" ? 2.5 : 3.5))))));
+            let right = square.substring(1,2);
+            let y = right == "1" ? 3.5 :
+            (right == "2" ? 2.5 :
+             (right == "3" ? 1.5 :
+              (right == "4" ? 0.5 :
+               (right == "5" ? -0.5 :
+                (right == "6" ? -1.5 :
+                 (right == "7" ? -2.5 : -3.5))))));
+            //if you're black, invert them
+            if (color == "black"){x = -x;y = -y;}
+            return [x,y];
+        }
+
+        var puzzle = function(){
             let gamePuzzle = new Chess();
             let moves = $('move');
             for(let i=0;i<moves.length;i++){
@@ -116,16 +120,9 @@ setTimeout(function() {
         return
     }
 
-    //Update game board
-    try {
-        let moves = $('kwdb');
-        for(let i=0;i<moves.length;i++){
-            game.move(moves[i].textContent);
-        }
-    } catch {
-        console.log("No one made a move yet");
-    }
-    stockfish.postMessage('position fen '+game.fen());
+
+    //You can load an opening book with this (may not be working)
+    //stockfish.postMessage('setoption name BookFile value https://raw.githubusercontent.com/mchappychen/lichess-funnies/main/Human.bin');
 
     //sends message to chat
     function send(x) {
@@ -137,104 +134,65 @@ setTimeout(function() {
             element.dispatchEvent(event);
         } catch {console.log('Error, couldn\'t send message')}
     }
-    
-    var autoHint = false;
-    var lastMove = 0;
-    let firstMove = false;
+
+    var autoHint = autoRun == "1";
     const moveObserver = new MutationObserver((mutationsList) => {
         for (const mutation of mutationsList) {
-            if (mutation.type === 'childList' && mutation.target.tagName == 'L4X' && mutation.target.children[mutation.target.children.length-1]) {
-                //Get the first move here
-                if(!firstMove){
-                    firstMove = true;
-                    game.move($('kwdb')[0].textContent);
-                }
+            if(mutation.addedNodes.length == 0){return}
+            if( mutation.addedNodes[0].tagName == "I5Z"){continue}
+            //$('g')[0].innerHTML = ''; //removes board arrows/circles
+            //If game is over
 
-                //$('g')[0].innerHTML = ''; //removes board arrows/circles
-                //If game is over
-                if((lastMove == mutation.target.children[mutation.target.children.length-1] || mutation.target.children[mutation.target.children.length-1].tagName != "KWDB") && $('.cg-wrap')[0].classList[1] == 'orientation-white'){
-                    //lichess.socket.send( "rematch-yes" );
-                    try{
-                        console.log("rematching...");
-                        send("Finding new opponent in 8 seconds...");
-                        lichess.socket.send( "rematch-yes" );
-                        setTimeout(function(){$('a.fbt[href^="/#pool"]')[0].click()},8000);
-                    }catch{
-                    console.log("Error");
-                    }
-                    return
-                }
+            //Get last move
+            let lastMove = $('l4x')[0].lastChild;
 
-                //Get last move
-                lastMove = mutation.target.children[mutation.target.children.length-1];
-                //console.log(lastMove);
+            //Update our game board
+            game.move(lastMove.textContent);
+            //If we're white, only get black moves. Vice versa
+            if ($('.cg-wrap')[0].classList[1] == 'orientation-white' && mutation.target.children.length%3!=0){
+                return
+            }
+            if ($('.cg-wrap')[0].classList[1] == 'orientation-black' && mutation.target.children.length%3==0){
+                return
+            }
 
-                //Update our game board
-                game.move(lastMove.textContent);
-                //console.log(game.history());
-
-                //If we're white, only get black moves. Vice versa
-                if ($('.cg-wrap')[0].classList[1] == 'orientation-white' && mutation.target.children.length%3!=0){
-                    return
-                }
-                if ($('.cg-wrap')[0].classList[1] == 'orientation-black' && mutation.target.children.length%3==0){
-                    return
-                }
-
-                if (autoHint){
-                    stockfish.postMessage('position fen '+game.fen());
-                    var time = $("div.time")[1].innerText.split("\n");
-                    if (game.history().length < 12){
-                        stockfish.postMessage('go movetime 20');
-                    } else if (parseInt(time[0])*60 + parseInt(time[2]) < 1){
-                        stockfish.postMessage('go movetime 20');
-                    } else if (parseInt(time[0])*60 + parseInt(time[2]) < 3){
-                        stockfish.postMessage('go movetime 200');
-                    } else {
-                        stockfish.postMessage('go movetime 400');
-                    }
+            if (autoHint){
+                stockfish.postMessage('position fen '+game.fen());
+                var time = $("div.time")[1].innerText.split("\n");
+                if (game.history().length < 16){
+                    stockfish.postMessage('go depth 9');
+                } else if (parseInt(time[0])*60 + parseInt(time[2]) < 2){
+                    stockfish.postMessage('go depth 5 movetime 50');
+                } else {
+                    stockfish.postMessage('go depth 12');
                 }
             }
         }
+
     });
     moveObserver.observe($('rm6')[0], {childList: true,subtree: true});
 
-
-    //Returns piece name given its abbreviation
-    function piece(p){
-        return p=='p' ? 'Pawn' :
-        (p=='r' ? 'Rook' :
-         (p=='n' ? 'Knight' :
-          (p=='b' ? 'Bishop' :
-           (p=='q' ? 'Queen' :
-            (p=='k' ? 'King' :
-             p)))));
-    }
+    const endObserver = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+            if($('div.rcontrols')[0].textContent.includes("Rematch")){
+                lichess.socket.send( "rematch-yes" );
+                send("GG. Finding new opponent in 8 seconds...");
+                setTimeout(function(){$('a.fbt[href^="/#pool"]')[0].click()},8000);
+                endObserver.disconnect();
+            }
+        }
+    });
+    endObserver.observe($('div.rcontrols')[0],{childList: true,subtree: true})
 
     stockfish.onmessage = function(event) {
         if(event.data.substring(0,8) == "bestmove"){
-            //console.log(`socket.rep.${Math.round(Date.now() / 1e3 / 3600 / 3)}`,`socket.rep.${Math.round(Date.now() / 1e3 / 3600 / 3)}`.nodeType,D(`socket.rep.${Math.round(Date.now() / 1e3 / 3600 / 3)}`));
             let bestMove = event.data.split(" ")[1];
-            //Remove all arrows
-            lichess.socket.averageLag = 1200;
-            if($('.cg-wrap')[0].classList[1] == 'orientation-white'){
-                //let arrowCoords1 = getArrowCoords(bestMove.substring(0,2),'white');
-                //let arrowCoords2 = getArrowCoords(bestMove.substring(2),'white');
-                //$('g')[0].innerHTML += '<line stroke="#15781B" stroke-width="0.2" stroke-linecap="round" marker-end="url(#arrowhead-g)" opacity="1" x1="'+arrowCoords1[0]+'" y1="'+arrowCoords1[1]+'" x2="'+arrowCoords2[0]+'" y2="'+arrowCoords2[1]+'"></line>';
-                //$('g')[0].innerHTML += '<circle stroke="#15781B" stroke-width="0.07" fill="lime" opacity="0.8" cx="'+arrowCoords1[0]+'" cy="'+arrowCoords1[1]+'" r="0.4" ></circle>';
-                //$('g')[0].innerHTML += '<circle stroke="#15781B" stroke-width="0.07" fill="red" opacity="0.6" cx="'+arrowCoords2[0]+'" cy="'+arrowCoords2[1]+'" r="0.4" ></circle>';
-                //$('defs')[0].innerHTML += '<marker id="arrowhead-g" orient="auto" markerWidth="4" markerHeight="8" refX="2.05" refY="2" cgKey="g"><path d="M0,0 V4 L3,2 Z" fill="#15781B"></path></marker>';
-
-                lichess.socket.send('move', { u: bestMove }, { ackable: true, sign:lichess.socket._sign, withLag:true });
-
-            } else {
-                lichess.socket.send('move', { u: bestMove }, { ackable: true, sign:lichess.socket._sign, withLag:true });
-            }
-            //console.log('averageLag',lichess.socket.averageLag);
-
+            lichess.socket.averageLag = 1200; // trick lichess to give us more time thinking we're lagging
+            lichess.socket.send('move', { u: bestMove }, { ackable: true, sign:lichess.socket._sign, withLag:true }); //sign:lichess.socket._sign tricks lichess so our socket won't get destroyed? Or is it cause we're shadowbanned?
+            //Note - according to lichess.socket code:
+            //we may need to delete socket.rep.${Math.round(Date.now() / 3600 / 3)} from localStorage each time we call lichess.socket.send() to avoid disconnects
         }
     };
-
 
     //Add stockfish hint button
     let sfbutton = document.createElement('button');
@@ -242,7 +200,7 @@ setTimeout(function() {
     sfbutton.classList.add('fbt');
     sfbutton.onclick=function(){
         stockfish.postMessage('position fen '+game.fen());
-        stockfish.postMessage('go depth 12');
+        stockfish.postMessage('go depth 10');
     }
     $('div .ricons')[0].appendChild(sfbutton);
 
@@ -252,11 +210,12 @@ setTimeout(function() {
     auHintButton.classList.add('fbt');
     auHintButton.onclick = function(){
         auHintButton.innerText = auHintButton.innerText == "AUTO-OFF" ? "Auto-ON" : "Auto-OFF";
-        autoHint = !autoHint;
         auHintButton.style.backgroundColor = autoHint ? "green" : "";
+        autoHint = !autoHint;
+        autoRun = autoRun=="1" ? "0" : "1";
+        localStorage.setItem('autorun',autoRun);
     }
     $('div .ricons')[0].appendChild(auHintButton);
-    auHintButton.click();
 
     //Make all buttons have a blue border when clicked
     $('.fbt').on('mousedown',function(){
@@ -266,15 +225,28 @@ setTimeout(function() {
         },500,this);
     });
 
-    //Make bongcloud opening
-    if($('.cg-wrap')[0].classList[1] == 'orientation-white'){
-        setTimeout(function(){lichess.socket.send('move', { u: 'e2e4' }, { ackable: true, sign:lichess.socket._sign, withLag:true });},1000);
-    } else {
-        setTimeout(function(){lichess.socket.send('move', { from: 'e7', to:'e5' }, { ackable: true, sign:lichess.socket._sign, withLag:true });},4000);
+    $(document).on("keydown", function(event) {
+        if (event.key == "w") {
+            sfbutton.click();
+            auHintButton.click();
+        }
+    });
+
+    if(autoRun == "1"){
+        auHintButton.innerText = auHintButton.innerText == "AUTO-OFF" ? "Auto-ON" : "Auto-OFF";
+        auHintButton.style.backgroundColor = autoHint ? "green" : "";
+        if($('.cg-wrap')[0].classList[1] == 'orientation-white'){
+            sfbutton.click();
+        } else {
+            let moves = $('kwdb');
+            for(let i=0;i<moves.length;i++){
+                game.move(moves[i].textContent.replace('✓',''));
+            }
+            stockfish.postMessage('position fen '+game.fen());
+            sfbutton.click();
+        }
     }
 
-
-},600);
-
-
+}
+setTimeout(run,500);
 
