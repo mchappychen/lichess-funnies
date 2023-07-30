2b6ec8b78445daee0fef3b9b2017d74c77f41ea1
Michael <51720724+mchappychen@users.noreply.github.com>
2023-07-29 23:06:42 -0400

    Update commentaryWorker.js

--- commentaryWorker.js
+++ commentaryWorker.js
@@ -1,7 +1,25 @@
-// Add an event listener to receive messages from the main thread
-self.onmessage = function(event) {
-  console.log('Message from Main Thread:', event.data);
+// Contents of the worker.js script
+const workerScript = `
+  self.onmessage = function(event) {
+    console.log('Message from Main Thread:', event.data);
+    self.postMessage('Hello from Web Worker!');
+  };
+`;
 
-  // Send a response back to the main thread
-  self.postMessage('Hello from Web Worker!');
+// Use the Blob URL to create a web worker
+const worker = new Worker(URL.createObjectURL(new Blob([workerScript], { type: 'application/javascript' })));
+
+// Add an event listener to receive messages from the worker
+worker.onmessage = function(event) {
+  console.log('Message from Web Worker:', event.data);
 };
+
+// Post a message to the worker
+worker.postMessage('Hello from Main Thread!');
+
+// Don't forget to revoke the Blob URL when it's no longer needed
+URL.revokeObjectURL(blobUrl);
+
+
+
+
